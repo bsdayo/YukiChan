d881ba2f155b1324520e49a5bfacbf90b76cb874
b1acksoil <bs@blacksoil.top>
2022-06-12 23:27:21 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 dotnet publish --nologo -r win-x64 --no-self-contained -o ./Build/Windows/ ./YukiChan/YukiChan.csproj
-dotnet publish --nologo -r linux-x64 --self-contained -o ./Build/Linux/ ./YukiChan/YukiChan.csproj
+dotnet publish --nologo -r linux-x64 --no-self-contained -o ./Build/Linux/ ./YukiChan/YukiChan.csproj
 
 rm ./Build/**/*.pdb
 rm ./Build/**/Konata*
