ce115f071a5a4ac064e245a0111879b24af34eee
b1acksoil <bs@blacksoil.top>
2022-08-04 13:39:22 +0800

    Update YukiChan.Tools structure

---
+++ YukiChan.Tools/Commands/Arcaea/Arcaea.cs
@@ -0,0 +1,22 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.IO.Compression;
+using System.Text;
+using SQLite;
+using YukiChan.Modules.Arcaea.Models;
+
+namespace YukiChan.Tools.Commands.Arcaea;
+
+[Command(
+    Name = "arcaea",
+    FullName = "Arcaea",
+    Description = "Arcaea Tools")]
+[Subcommand(
+    typeof(UnpackAssets),
+    typeof(UpdateSongDb))]
+public class ArcaeaCommand
+{
+    public void OnExecute(CommandLineApplication app)
+    {
+        app.ShowHelp();
+    }
+}
--- YukiChan.Tools/Program.cs
+++ YukiChan.Tools/Program.cs
@@ -1,5 +1,5 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
-using YukiChan.Tools.Commands;
+using YukiChan.Tools.Commands.Arcaea;
 
 namespace YukiChan.Tools;
 
@@ -9,7 +9,7 @@ public class Program
 {
     public static int Main(string[] args)
         => CommandLineApplication.Execute<Program>(args);
-    
+
     private void OnExecute(CommandLineApplication app)
     {
         app.ShowHelp();
