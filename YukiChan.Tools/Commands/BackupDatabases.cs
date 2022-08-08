3e15a4725c6718cd873ad3641174bca4966661d4
b1acksoil <bs@blacksoil.top>
2022-08-08 14:35:10 +0800

    Add database backup

---
+++ YukiChan.Tools/Commands/BackupDatabases.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Compression;
+using System.Text;
+using McMaster.Extensions.CommandLineUtils;
+using YukiChan.Utils;
+
+namespace YukiChan.Tools.Commands;
+
+[Command(
+    Name = "backup-databases",
+    FullName = "Backup Databases",
+    Description = "Backup all YukiChan's Databases")]
+[HelpOption]
+public class BackupDatabases
+{
+    public void OnExecute(CommandLineApplication app)
+    {
+        if (!Directory.Exists("DatabaseBackups"))
+            Directory.CreateDirectory("DatabaseBackups");
+        if (!Directory.Exists("Temp/DatabasesCopy"))
+            Directory.CreateDirectory("Temp/DatabasesCopy");
+
+        foreach (var db in Directory.GetFiles("Databases"))
+            File.Copy(db, $"Temp/DatabasesCopy{db.RemoveString("Databases")}", true);
+
+
+        var today = DateTime.Today;
+        var path = $"DatabaseBackups/{today.Year}-{today.Month}-{today.Day}.zip";
+
+        if (File.Exists(path))
+            File.Delete(path);
+
+        ZipFile.CreateFromDirectory("Temp/DatabasesCopy", path,
+            CompressionLevel.Optimal, false, Encoding.UTF8);
+
+        Console.WriteLine($"Databases backuped to {path}.");
+    }
+}
--- YukiChan.Tools/Program.cs
+++ YukiChan.Tools/Program.cs
@@ -1,14 +1,18 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
+using YukiChan.Tools.Commands;
 using YukiChan.Tools.Commands.Arcaea;
 
 namespace YukiChan.Tools;
 
 [Subcommand(typeof(ArcaeaCommand))]
+[Subcommand(typeof(BackupDatabases))]
 [HelpOption]
 public class Program
 {
     public static int Main(string[] args)
-        => CommandLineApplication.Execute<Program>(args);
+    {
+        return CommandLineApplication.Execute<Program>(args);
+    }
 
     private void OnExecute(CommandLineApplication app)
     {
