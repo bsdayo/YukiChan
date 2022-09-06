b2f4657769cd6e2af7170627d8b826107067fd91
b1acksoil <dev@sorabs.cc>
2022-09-06 22:10:21 +0800

    Update phrase log

--- YukiChan/Modules/Phrase/Phrase.cs
+++ YukiChan/Modules/Phrase/Phrase.cs
@@ -12,29 +12,40 @@ namespace YukiChan.Modules.Phrase;
     Version = "1.3.0")]
 public class Phrase : ModuleBase
 {
+    private static readonly ModuleLogger Logger = new("Phrase");
+    
     [Command("Generate Phrase",
         Description = "生成短语",
         FallbackCommand = true)]
     public static MessageBuilder Generate(Bot bot, MessageStruct message, string body)
     {
-        var sb = new StringBuilder();
+        try
+        {
+            var sb = new StringBuilder();
 
-        sb.Append(Global.YukiDb.GetPhraseWord("time"));
+            sb.Append(Global.YukiDb.GetPhraseWord("time"));
 
-        sb.Append(Global.YukiDb.GetPhraseWord("adj"));
-        sb.Append(Global.YukiDb.GetPhraseWord("noun"));
+            sb.Append(Global.YukiDb.GetPhraseWord("adj"));
+            sb.Append(Global.YukiDb.GetPhraseWord("noun"));
 
-        sb.Append(Global.YukiDb.GetPhraseWord("place"));
+            sb.Append(Global.YukiDb.GetPhraseWord("place"));
 
-        sb.Append(new Random().Next() % 2 == 0 ? '把' : '被');
+            sb.Append(new Random().Next() % 2 == 0 ? '把' : '被');
 
-        sb.Append(Global.YukiDb.GetPhraseWord("adj"));
-        sb.Append(Global.YukiDb.GetPhraseWord("noun"));
+            sb.Append(Global.YukiDb.GetPhraseWord("adj"));
+            sb.Append(Global.YukiDb.GetPhraseWord("noun"));
 
-        sb.Append(Global.YukiDb.GetPhraseWord("adv"));
-        sb.Append(Global.YukiDb.GetPhraseWord("verb"));
+            sb.Append(Global.YukiDb.GetPhraseWord("adv"));
+            sb.Append(Global.YukiDb.GetPhraseWord("verb"));
 
-        return message.Reply(sb.ToString());
+            return message.Reply(sb.ToString());
+
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+            return message.Reply($"发生错误！({e.GetType().Name}: {e.Message})");
+        }
     }
 
     [Command("Add Phrase Word",
