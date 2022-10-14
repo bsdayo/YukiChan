e09b468c0c3b828229b221ffdacf6db39774feb0
b1acksoil <dev@sorabs.cc>
2022-10-15 03:39:45 +0800

    refactor: cleanup project directory

--- build-all.sh
+++
@@ -1,3 +0,0 @@
-./build-windows-amd64.sh
-./build-linux-amd64.sh
-./build-linux-arm64.sh
--- build-linux-amd64.sh
+++
@@ -1,9 +0,0 @@
-BUILD_DIR="linux-amd64"
-
-dotnet publish --nologo -r linux-x64 --no-self-contained -o ./Build/${BUILD_DIR}/ ./YukiChan/YukiChan.csproj
-dotnet publish --nologo -r linux-x64 --no-self-contained -o ./Build/${BUILD_DIR}/ ./YukiChan.Tools/YukiChan.Tools.csproj
-
-rm ./Build/${BUILD_DIR}/*.pdb
-rm ./Build/${BUILD_DIR}/*.xml
-rm ./Build/${BUILD_DIR}/*.deps.json
-rm ./Build/${BUILD_DIR}/*.runtimeconfig.json
