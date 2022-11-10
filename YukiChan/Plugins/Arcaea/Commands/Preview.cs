99b5bbd30fcd43e1f2616214d234ca311898b816
b1acksoil <dev@sorabs.cc>
2022-11-10 22:01:47 +0800

    feat(arcaea): add preview/random command

---
+++ YukiChan/Plugins/Arcaea/Commands/Preview.cs
@@ -0,0 +1,59 @@
+﻿using ArcaeaUnlimitedAPI.Lib.Models;
+using ArcaeaUnlimitedAPI.Lib.Utils;
+using Flandre.Core.Attributes;
+using Flandre.Core.Common;
+using Flandre.Core.Messaging;
+using YukiChan.Utils;
+
+// ReSharper disable CheckNamespace
+
+namespace YukiChan.Plugins.Arcaea;
+
+public partial class ArcaeaPlugin
+{
+    [Command("a.preview <songname: string> [difficulty: string = ftr]")]
+    [Shortcut("查预览")]
+    public async Task<MessageContent> OnPreview(MessageContext ctx, ParsedArgs args)
+    {
+        var difficulty = ArcaeaUtils.GetRatingClass(args.GetArgument<string>("difficulty"));
+        if (difficulty is null)
+            return ctx.Reply("输入了错误的难度！");
+
+        var songId = await ArcaeaSongDatabase.FuzzySearchId(args.GetArgument<string>("songname"));
+        if (songId is null) return ctx.Reply("没有找到该曲目呢...");
+
+        try
+        {
+            byte[] preview;
+            var cachePath = $"{YukiDir.ArcaeaCache}/preview/{songId}-{difficulty.ToString()!.ToLower()}.jpg";
+
+            try
+            {
+                preview = await File.ReadAllBytesAsync(cachePath);
+            }
+            catch
+            {
+                preview = await _auaClient.Assets.Preview(songId, AuaSongQueryType.SongId, difficulty.Value);
+                await File.WriteAllBytesAsync(cachePath, preview);
+                Logger.SaveCache(cachePath);
+            }
+
+            return ctx.Reply().Image(preview);
+        }
+        catch (AuaException e)
+        {
+            var errMsg = AuaErrorStatus.GetMessage(e.Status, e.Message);
+            return ctx.Reply(errMsg);
+        }
+        catch (YukiException e)
+        {
+            Logger.Error(e);
+            return ctx.Reply(e.Message);
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+            return ctx.Reply($"发生了奇怪的错误！({e.Message})");
+        }
+    }
+}
---
+++ YukiChan/Plugins/Arcaea/Commands/Random.cs
@@ -0,0 +1,97 @@
+﻿using ArcaeaUnlimitedAPI.Lib.Models;
+using Flandre.Core.Attributes;
+using Flandre.Core.Common;
+using Flandre.Core.Messaging;
+using YukiChan.Plugins.Arcaea.Models;
+using YukiChan.Utils;
+
+// ReSharper disable CheckNamespace
+
+namespace YukiChan.Plugins.Arcaea;
+
+public partial class ArcaeaPlugin
+{
+    [Command("a.random [range:text]")]
+    [Shortcut("随机曲目")]
+    public async Task<MessageContent> OnRandom(MessageContext ctx, ParsedArgs args)
+    {
+        var range = args.GetArgument<string>("range")
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        int start, end;
+        var allCharts = ArcaeaSongDatabase.GetAllCharts();
+
+        try
+        {
+            switch (range.Length)
+            {
+                // 不提供参数，全曲 Future 难度随机
+                case 0:
+                    return await ConstructRandomReply(ctx, allCharts
+                        .Where(chart => chart.RatingClass == (int)ArcaeaDifficulty.Future)
+                        .ToArray());
+
+                // 提供定数范围
+                case 1:
+                    start = ArcaeaUtils.GetRatingRange(range[0]).Start;
+                    end = ArcaeaUtils.GetRatingRange(range[0]).End;
+
+                    if (start == -1 || end == -1)
+                        return ctx.Reply("输入了错误的定数呢...");
+                    if (start is < 0 or > 120 || end is < 0 or > 120)
+                        return ctx.Reply("定数超出范围啦！");
+
+                    return await ConstructRandomReply(ctx, allCharts
+                        .Where(chart => chart.Rating >= start && chart.Rating <= end)
+                        .ToArray());
+
+                // 提供最低和最高定数
+                case 2:
+                    start = ArcaeaUtils.GetRatingRange(range[0]).Start;
+                    end = ArcaeaUtils.GetRatingRange(range[1]).End;
+
+                    if (start == -1 || end == -1)
+                        return ctx.Reply("输入了错误的定数呢...");
+                    if (start > end)
+                        return ctx.Reply("最低定数比最高定数大诶...");
+                    if (start is < 0 or > 120 || end is < 0 or > 120)
+                        return ctx.Reply("定数超出范围啦！");
+
+                    return await ConstructRandomReply(ctx, allCharts
+                        .Where(chart => chart.Rating >= start && chart.Rating <= end)
+                        .ToArray());
+
+                default:
+                    return ctx.Reply("参数太多啦！");
+            }
+        }
+        catch (YukiException e)
+        {
+            Logger.Error(e);
+            return ctx.Reply(e.Message);
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+            return ctx.Reply($"发生了奇怪的错误！({e.Message})");
+        }
+    }
+
+    private async Task<MessageBuilder> ConstructRandomReply(MessageContext ctx, ArcaeaSongDbChart[] allCharts)
+    {
+        if (allCharts.Length == 0)
+            return ctx.Reply("该定数范围内没有曲目哦~");
+
+        var chart = allCharts[new Random().Next(allCharts.Length)];
+
+        var songCover = await _auaClient.GetSongCover(
+            chart.SongId, chart.JacketOverride, (ArcaeaDifficulty)chart.RatingClass);
+
+        return ctx.Reply()
+            .Text("随机推荐曲目：\n")
+            .Image(songCover)
+            .Text($"{chart.NameEn}\n")
+            .Text($"({ArcaeaSongDatabase.GetPackageBySet(chart.Set)!.Name})\n")
+            .Text(
+                $"{(ArcaeaDifficulty)chart.RatingClass} {chart.Rating.GetDifficulty()} [{((double)chart.Rating / 10).ToString("0.0")}]");
+    }
+}
