ed6d2c4ad93518d95055fecf4b7e93e168d6966f
b1acksoil <dev@sorabs.cc>
2022-12-11 01:25:01 +0800

    feat: update build scripts

--- scripts/build-linux-arm64.sh
+++ scripts/build-linux-arm64.sh
@@ -1,5 +1,6 @@
 # Build
 dotnet publish -c Release -r linux-arm64 --no-self-contained -o ./build/linux-arm64/ ./src/YukiChan/YukiChan.csproj
+dotnet publish -c Release -r linux-arm64 --no-self-contained -o ./build/linux-arm64/ ./src/YukiChan.Tools/YukiChan.Tools.csproj
 
 # Cleanup
 rm ./build/linux-arm64/*.xml
