59833d2eb69ae8fa5a3f7bdaa28a6790f128f06d
b1acksoil <dev@sorabs.cc>
2022-12-21 20:42:34 +0800

    scripts: update powershell scripts

---
+++ scripts/build-linux-arm64.ps1
@@ -0,0 +1,9 @@
+$TARGET = "linux-arm64"
+
+# Build
+dotnet publish -c Release -r $TARGET --no-self-contained -o "./build/$TARGET/" ./src/YukiChan/YukiChan.csproj
+dotnet publish -c Release -r $TARGET --no-self-contained -o "./build/$TARGET/" ./src/YukiChan.Tools/YukiChan.Tools.csproj
+
+# Cleanup
+rm ./build/win-x64/*.xml
+rm ./build/win-x64/*.pdb
