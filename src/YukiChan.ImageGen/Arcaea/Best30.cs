e80224b233d58f379f80d8b18ea1f049ab0699de
b1acksoil <dev@sorabs.cc>
2022-12-25 10:22:23 +0800

    fix(arcaea): fix b30-dark rank color

--- src/YukiChan.ImageGen/Arcaea/Best30.cs
+++ src/YukiChan.ImageGen/Arcaea/Best30.cs
@@ -198,7 +198,7 @@ public partial class ArcaeaImageGenerator
                 Typeface = TitilliumWeb_SemiBold
             };
             using var rectPaint = new SKPaint();
-            textPaint.Color = SKColor.Parse(rank == 3 ? "#ffffff" : "#333333");
+            textPaint.Color = SKColor.Parse(rank == 3 ? "#ffffff" : (dark ? "#ffffff" : "#333333"));
             rectPaint.Color = SKColor.Parse(rank switch
             {
                 1 => "#ffcc00",
