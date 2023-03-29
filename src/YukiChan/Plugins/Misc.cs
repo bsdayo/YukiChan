24be736ca4e4fba488f25d4d4e98a95f4a57a0cf
bsdayo <dev@sorabs.cc>
2023-03-29 20:27:14 +0800

    feat(misc): add choose command

---
+++ src/YukiChan/Plugins/Misc.cs
@@ -0,0 +1,21 @@
+﻿using Flandre.Core.Messaging;
+using Flandre.Framework.Attributes;
+using Flandre.Framework.Common;
+using YukiChan.Utils;
+
+namespace YukiChan.Plugins;
+
+public class MiscPlugin : Plugin
+{
+    [Command]
+    [StringShortcut("帮助")]
+    public string Help() => "[暮雪酱帮助文档]\nhttps://yukidocs.sorabs.cc/";
+
+    [Command]
+    [StringShortcut("帮我选")]
+    public MessageContent Choose(CommandContext ctx, params string[] items)
+    {
+        var result = items[new Random().Next(items.Length)];
+        return ctx.Reply("建议你选择{result}");
+    }
+}
